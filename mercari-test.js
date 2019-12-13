db4d0f94a308e1bf38971596dda21e6d2a32ec19
al <alifzulkifeli@gmail.com>
2019-12-13 08:48:34 +0000

    gh

--- mercari-test.js
+++ mercari-test.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-	  const browser = await puppeteer.launch({args: ['--no-sandbox']});
+	  const browser = await puppeteer.launch({args: ['--no-sandbox',
+	      '--disable-setuid-sandbox',],});
 	  const page = await browser.newPage();
 	  await page.goto('http://mercari.com/jp');
 	  await page.screenshot({path: 'example.png'});
